00def326c22f03642ab5448b7a89c1e00113280c
CalebJones <railrider1@verizon.net>
2016-10-15 17:39:59 -0400

    Update Controller_v1.0.ino

--- Controller_v1.0.ino
+++ Controller_v1.0.ino
@@ -1,6 +1,7 @@
 #include <BMI160.h>
 #include <CurieIMU.h>
 
+double totX=0, totY=0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,7 +9,6 @@ void setup() {
   Serial.begin(9600);
   CurieIMU.autoCalibrateGyroOffset();
   CurieIMU.setGyroRate(100);
-  double totX=0, totY=0;
 }
 
 void loop() {
@@ -18,12 +18,12 @@ void loop() {
   CurieIMU.readGyro(rawX, rawY, trash);
   X=(rawX/32768.9)*CurieIMU.getGyroRange();
   Y=(rawY/32768.9)*CurieIMU.getGyroRange();
-  if (-10<X && X<10)
+  if (-3<X && X<3)
     X=0;
-  if (-10<Y && Y<10)
+  if (-3<Y && Y<3)
     Y=0;
-  totX+=X;
-  totY+=Y;
+  totX+=(X*.01);
+  totY+=(Y*.01);
   Serial.print(totX);
   Serial.print(" ");
   Serial.println(totY);
